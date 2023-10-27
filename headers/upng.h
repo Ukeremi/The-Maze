5120b3dfd269a8d556dd185ea315330cc4808d9b
Ukpono <ukpono9@gmail.com>
2023-10-27 02:45:30 -0700

    update on upng header

--- headers/upng.h
+++ headers/upng.h
@@ -1,4 +1,3 @@
-
 #ifndef UPNG_H
 #define UPNG_H
 /**
@@ -49,3 +48,31 @@ typedef enum upng_format
 	UPNG_RGBA16,
 	UPNG_LUMINANCE1,
 	UPNG_LUMINANCE2,
+	UPNG_LUMINANCE_ALPHA4,
+        UPNG_LUMINANCE_ALPHA8
+} upng_format;
+
+typedef struct upng_t upng_t;
+
+upng_t *upng_new_from_bytes(const unsigned char *buffer, unsigned long size);
+upng_t *upng_new_from_file(const char *path);
+void upng_free(upng_t *upng);
+
+upng_error upng_header(upng_t *upng);
+upng_error upng_decode(upng_t *upng);
+
+upng_error upng_get_error(const upng_t *upng);
+unsigned int upng_get_error_line(const upng_t *upng);
+
+unsigned int upng_get_width(const upng_t *upng);
+unsigned int upng_get_height(const upng_t *upng);
+unsigned int upng_get_bpp(const upng_t *upng);
+unsigned int upng_get_bitdepth(const upng_t *upng);
+unsigned int upng_get_components(const upng_t *upng);
+unsigned int upng_get_pixelsize(const upng_t *upng);
+upng_format     upng_get_format(const upng_t *upng);
+
+const unsigned char *upng_get_buffer(const upng_t *upng);
+unsigned int upng_get_size(const upng_t *upng);
+
+#endif /*defined(UPNG_H)*/
